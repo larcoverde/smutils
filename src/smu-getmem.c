eb767194da86cd50ee46616a7141534794b19d40
Lucas Arcoverde <larcoverde@proton.me>
2026-07-08 20:48:26 -0300

    feat(getmem): add flags '--total', '--used', '--free'.

--- src/smu-getmem.c
+++ src/smu-getmem.c
@@ -7,6 +7,7 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 long mem_total, mem_free;
 
@@ -34,19 +35,34 @@ void get_memory()
 int main(int argc, char** argv)
 {
     char *program_name = argv[0];
+    get_memory();
+    long mem_used = mem_total - mem_free;
+
     if (argc > 2)
     {
         usage(program_name);
         return 1;
     }
+    else if (argc == 1)
+    {
+        printf("total: %ld\n", mem_total);
+        printf("used:  %ld\n", mem_used);
+        printf("free:  %ld\n", mem_free);
+        return 0;
+    }
 
-    get_memory();
-
-    long mem_used = mem_total - mem_free;
-
-    printf("total: %ld\n", mem_total);
-    printf("used:  %ld\n", mem_used);
-    printf("free:  %ld\n", mem_free);
+    if (strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "--total") == 0)
+    {
+        printf("total: %ld\n", mem_total);
+    }
+    else if (strcmp(argv[1], "-u") == 0 || strcmp(argv[1], "--used") == 0)
+    {
+        printf("used:  %ld\n", mem_used);
+    } 
+    else if (strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "--free") == 0)
+    {
+        printf("free:  %ld\n", mem_free);
+    }
 
     return 0;
 }
