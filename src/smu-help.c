ff77a37f203c9280fca1e764287ea86381d60940
Lucas Arcoverde <lucasmarcoverde@gmail.com>
2026-07-08 21:03:11 -0300

    feat(help): create help message for getmem.

--- src/smu-help.c
+++ src/smu-help.c
@@ -14,6 +14,22 @@ void help_clear()
     printf("source code -> smutils/src/smu-clear.c");
 }
 
+void help_getmem()
+{
+    printf("SMU - getmem\n");
+    printf("version 1.1\n");
+    printf("display memory information.\n");
+    printf("usage:\n");
+    printf("      getmem <flag>\n");
+    printf("      flags:\n");
+    printf("            -t --total        display only total memory.\n");
+    printf("            -u --used         display only used memory.\n");
+    printf("            -f --free         display only free memory.\n\n");
+    printf("             use NO flags to get all information.\n\n");
+    printf("      at this moment smu-getmem only accept one flag at time.\n\n");
+    printf("source code -> smutils/src/smu-getmem.c");
+}
+
 int main(int argc, char **argv)
 {
     char *program = argv[1];
@@ -21,7 +37,7 @@ int main(int argc, char **argv)
     if (strcmp(program, "clear") == 0)
         help_clear();
     else if (strcmp(program, "getmem") == 0)
-        //help_getmem();
+        help_getmem();
 
     return 0;
 }
